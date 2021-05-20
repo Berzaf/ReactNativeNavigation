739dc93bf8c4f505f9185b69cc2c63ef57603f16
Berzaf <berzafastroo2012@gmail.com>
2021-05-19 20:44:52 -0500

    bottomTab

---
+++ Navigators/BottomTabNavigation.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function MyTabs() {
+    return(
+        <View></View>
+    )
+}
+
+export default function TabNavigation() {
+    return(
+        
+    )
+}
