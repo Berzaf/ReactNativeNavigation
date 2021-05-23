a5b0c71ab124d968c4ef04596876d16670c2dc63
Berzaf <berzafastroo2012@gmail.com>
2021-05-22 22:53:58 -0500

    Routes Param Navigator

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import BottomTabNavigation from './Navigators/BottomTabNavigation'
 import TopTabNavigator from './Navigators/TopBarNavigator'
 import StackNavigation from './Navigators/StackNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RouteParamsNavigator from './Navigators/RouteParamsNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <BottomTabNavigation />
+      <RouteParamsNavigator />
+    
     </SafeAreaProvider>
   );
 }
